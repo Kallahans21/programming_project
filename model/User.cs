df186828ce696a86d77d84b24f811bab6f3eac45
josue.ayala <ayalavalencia11@gmail.com>
2021-04-08 12:50:27 -0600

    🚀 Variable initialization

--- model/User.cs
+++ model/User.cs
@@ -7,9 +7,9 @@ namespace programming_project.model
     class User
     {
         /* Public parameters to receive */
-        public string name;
-        public string document;
-        public string vehicle;
-        public double cost;
+        public string name = null;
+        public string document = null;
+        public string vehicle = null;
+        public double cost = 0;
     }
 }
