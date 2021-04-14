b472f0f87f4d1016c226a85957ec18398a45c07e
josue.ayala <ayalavalencia11@gmail.com>
2021-04-13 21:44:00 -0600

    🚀 Auth module

--- controllers/UserController.cs
+++ controllers/UserController.cs
@@ -7,8 +7,7 @@ namespace programming_project.controllers
     class UserController{
         
         public void init(){
-          Console.WriteLine("Initialization project");
-          getUsers();
+          auth();
         }
 
         public static void getUsers(){
@@ -18,6 +17,29 @@ namespace programming_project.controllers
           /* The list is created to be able to add objects */
           List<UserModel> userList = new List<UserModel>();
           userList.Add(user);
+        }
+
+        /* Authtentication function | method */
+        private static void auth(){
+          /* Test data */
+          string[] data = {"josue", "1234"};
+
+          string[] userData = new string[2];
+          
+          do
+          { 
+              Console.WriteLine("Enter the user");
+              userData[0] = Console.ReadLine();
+              Console.WriteLine("Enter the password");
+              userData[1] = Console.ReadLine();
+
+          } while (data[0] != userData[0] || data[1] != userData[1]);
+
+          test();
+        }
+
+        private static void test(){
+          Console.WriteLine("Te logeaste correctamente");
         }
     }
 }
