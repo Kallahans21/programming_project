1809e115ed3bf17d4919efba9b8d1cda0a63050f
josue.ayala <ayalavalencia11@gmail.com>
2021-04-14 22:02:35 -0600

    🙈 Register Integration

--- controllers/UserController.cs
+++ controllers/UserController.cs
@@ -1,45 +1,71 @@
 using System;
 using System.Collections.Generic;
 using programming_project.model;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace programming_project.controllers
 {
-    class UserController{
-        
-        public void init(){
-          auth();
-        }
+  class UserController
+  {
 
-        public static void getUsers(){
-          /* Instace of UserModel to get properties */
-          UserModel user = new UserModel();
+    private static string[] userInformation = new string[2];
 
-          /* The list is created to be able to add objects */
-          List<UserModel> userList = new List<UserModel>();
-          userList.Add(user);
-        }
-
-        /* Authtentication function | method */
-        private static void auth(){
-          /* Test data */
-          string[] data = {"josue", "1234"};
-
-          string[] userData = new string[2];
-          
-          do
-          { 
-              Console.WriteLine("Enter the user");
-              userData[0] = Console.ReadLine();
-              Console.WriteLine("Enter the password");
-              userData[1] = Console.ReadLine();
-
-          } while (data[0] != userData[0] || data[1] != userData[1]);
-
-          test();
-        }
-
-        private static void test(){
-          Console.WriteLine("Te logeaste correctamente");
-        }
+    public void init()
+    {
+      register();
     }
+
+    public static void getUsers()
+    {
+      /* Instace of UserModel to get properties */
+      UserModel user = new UserModel();
+
+      /* The list is created to be able to add objects */
+      List<UserModel> userList = new List<UserModel>();
+      userList.Add(user);
+    }
+
+    /* Authtentication function | method */
+    private static void auth()
+    {
+      /* Creation of array yo get user data */
+      string[] userData = new string[2];
+      string[] text = System.IO.File.ReadAllLines(@"database\user.txt");
+
+      bool authtenticated = false;
+      /* Validate if the information is correct */
+      do
+      {
+        Console.WriteLine("Enter username");
+        userData[0] = Console.ReadLine();
+        Console.WriteLine("Enter password");
+        userData[1] = Console.ReadLine();
+
+        foreach (var item in text)
+        {
+          if (item.Split('|')[0] == userData[0] && item.Split('|')[1] == userData[1])
+          {
+            userInformation[0] = item.Split('|')[0];
+            authtenticated = true;
+          }
+        }
+
+      } while (!authtenticated);
+
+      /* if the information is correct, go to the next method */
+      Console.WriteLine($"Tu usuario es {userInformation[0]}");
+    }
+
+    private static async void register()
+    {
+      string[] userData = new string[1];
+      Console.WriteLine("Enter username");
+      userData[0] += Console.ReadLine();
+      Console.WriteLine("Enter password");
+      userData[0] += $"|{Console.ReadLine()}";
+
+      await File.AppendAllLinesAsync(@"database\user.txt", userData);
+    }
+  }
 }
